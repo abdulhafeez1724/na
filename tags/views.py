7d762b7b01e3f33044b1ddc489262fbc43a6769f
abdul hafeez <32317154+abdulhafeez1724@users.noreply.github.com>
2024-11-19 11:46:59 +0500

    adding authentications

--- tags/views.py
+++ tags/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Tags
 from .serializers import TagsSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class TagsViewSet(ModelViewSet):
     queryset = Tags.objects.all()
     serializer_class = TagsSerializer
+    permission_classes = [IsAuthenticated]
