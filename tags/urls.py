d0e1ffd30292d8c326b318bc067ff546ad239aee
abdul hafeez <32317154+abdulhafeez1724@users.noreply.github.com>
2024-11-19 11:47:50 +0500

    fix urls

--- tags/urls.py
+++ tags/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TagsViewSet
 
-# Create a router and register the TagsViewSet with it
+
 router = DefaultRouter()
 router.register(r'tags', TagsViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),  # Include the router-generated URLs
+    path('', include(router.urls)),
 ]
