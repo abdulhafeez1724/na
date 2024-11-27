ebdbbf16aa65bd0c32d53baaffbc06d39768bcc6
abdul hafeez <32317154+abdulhafeez1724@users.noreply.github.com>
2024-11-27 16:52:04 +0500

    testing

--- server/server/settings.py
+++ server/server/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "tags",
+    #sdfsdf
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
